5c783e03d99b04c7cac831e1c0dda4f6dfedcbca
jkudirka <jkudirka@penlink.com>
2015-04-19 12:23:32 -0500

    Added scaffolding and some implementation.

---
+++ UserManagement/DesktopApp/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+           this.Close();
+        }
+
+        private void RefreshUsers(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddUser(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditUser(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteUser(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChangeUserPassword(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

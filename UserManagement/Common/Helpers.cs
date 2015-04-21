561d6d94bf5a6b99f6e6d6349945eeef7b485e03
jkudirka <jkudirka@penlink.com>
2015-04-20 19:39:37 -0500

    missed some files

---
+++ UserManagement/Common/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public static class Helpers
+    {
+        #region Constants
+        private const string Salt = "47DAB234-3CA8-42A4-A5B4-DA83D4228B1D";
+        #endregion
+
+        public static string HashPassword(string password, string salt = null)
+        {
+            byte[] bytes = null;
+            if (string.IsNullOrEmpty(salt))
+                salt = Salt;
+
+            using (var shaProvider = new System.Security.Cryptography.SHA1CryptoServiceProvider())
+            {
+                bytes = shaProvider.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password + salt));
+            }
+
+            return string.Concat(bytes.Select(b => b.ToString("x2")));
+        }
+    }
+}
---
+++ UserManagement/DesktopApp/LoginForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp
+{
+    public partial class LoginForm : Form
+    {
+        public string Username { get { return _Username.Text; } }
+        public string Password { get { return _Password.Text; } }
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Okay_Click(object sender, EventArgs e)
+        {
+
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
